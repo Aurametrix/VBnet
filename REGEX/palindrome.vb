d0e0d6b924883f3138a54d55a404c948142e1fb4
Aurametrix <irene@aurametrix.com>
2015-08-16 22:13:30 -0700

    Using While and If statement

--- REGEX/palindrome.vb
+++ REGEX/palindrome.vb
@@ -7,3 +7,16 @@ Private Sub CheckPalindrome(ByVal strString As String)
             MessageBox.show("This string is not Palindrome.")
         End If
     End Sub
+
+Public Function IsPalindrome(s As String) As Boolean
+ Dim i As Integer = 0
+ Dim j As Integer = s.Length - 1
+ While i < j
+  If s(i) <> s(j) Then
+   Return False
+  End If
+  i += 1
+  j -= 1
+ End While
+ Return True
+End Function
